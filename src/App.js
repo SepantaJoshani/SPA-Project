54722931c18f9e576dd23ee6cc85b07c06204662
Sepanta <Sepanta_97@Yahoo.com>
2021-08-16 04:09:31 +0430

    using-Route-BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Route } from "react-router";
+import Welcome from "./components/welcome";
+import Products from "./components/Products";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/Products">
+        <Products />
+      </Route>
     </div>
   );
 }
