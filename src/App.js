7e1558ddc2170c8e7f8c81f04093e2f51d64f3b8
Sepanta <Sepanta_97@Yahoo.com>
2021-08-17 00:45:32 +0430

    useParams

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
@@ -6,7 +6,10 @@ import QuoteDetail from "./pages/QuoteDetail";
 function App() {
   return (
     <Switch>
-      <Route path="/quotes">
+      <Route path="/" exact>
+    <Redirect to="/quotes"  />
+      </Route>
+      <Route path="/quotes" exact>
         <AllQuotes />
       </Route>
       <Route path="/quotes/:quoteId">
