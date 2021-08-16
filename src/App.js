a1ba416b6e7ee8f446409360b009d4dcc3d7df6a
Sepanta <Sepanta_97@Yahoo.com>
2021-08-17 00:05:55 +0430

    new-project-train

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-import { Route } from "react-router";
-import Welcome from "./pages/welcome";
-import Products from "./pages/Products";
-import MainHeader from "./components/MainHeader";
-import ProductDetail from "./pages/ProductDetail";
-import { Redirect, Switch } from "react-router-dom";
-
 function App() {
   return (
-    <div style={{textAlign:"center"}}>
-      <MainHeader />
-     <Switch>
-       <Route path="/" exact>
-         <Redirect to="/welcome" />
-       </Route>
-     <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products" exact>
-        <Products />
-      </Route>
-      <Route path="/products/:productId">
-    <ProductDetail />
-      </Route>
-     </Switch>
+    <div>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+import './index.css';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
