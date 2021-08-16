1572ec24bfd6c95814c4d4a1be7505072c515d8e
Sepanta <Sepanta_97@Yahoo.com>
2021-08-16 20:48:20 +0430

    redirects

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Welcome from "./pages/welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
 import ProductDetail from "./pages/ProductDetail";
-import { Switch } from "react-router-dom";
+import { Redirect, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div style={{textAlign:"center"}}>
       <MainHeader />
      <Switch>
+       <Route path="/" exact>
+         <Redirect to="/welcome" />
+       </Route>
      <Route path="/welcome">
         <Welcome />
       </Route>
