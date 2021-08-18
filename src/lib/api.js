3042f55993930cde2a0bd18044ae719b25178e4a
Sepanta <Sepanta_97@Yahoo.com>
2021-08-19 02:51:25 +0430

    sending-newcomment-data

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const FIREBASE_DOMAIN = 'https://react-prep-default-rtdb.firebaseio.com';
+const FIREBASE_DOMAIN = 'https://spa-project-28d5b-default-rtdb.firebaseio.com';
 
 export async function getAllQuotes() {
   const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`);
