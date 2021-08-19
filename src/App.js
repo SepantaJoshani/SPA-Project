b75e108609be7907abcbbc278839316e5b24a2c0
Sepanta <Sepanta_97@Yahoo.com>
2021-08-19 18:47:27 +0430

    creating Redirect Route & useparams

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import AllQuote from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
@@ -6,7 +6,10 @@ import QuoteDetail from "./pages/QuoteDetail";
 function App() {
   return (
    <Switch>
-     <Route path="/quotes"exact>
+     <Route path="/" exact>
+<Redirect to="/quotes" />
+     </Route>
+     <Route path="/quotes" exact>
     <AllQuote />
      </Route>
      <Route path="/quotes/:quoteId">
