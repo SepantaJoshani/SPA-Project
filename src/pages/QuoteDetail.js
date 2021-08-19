946d6dc241f8c30284dd5e287f6c25073807c88a
Sepanta <Sepanta_97@Yahoo.com>
2021-08-19 19:40:07 +0430

    displaying highlighted quote with find method

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,17 +1,29 @@
 import { Fragment } from "react";
-import { Route, useParams } from "react-router-dom"
-import Comments from '../components/comments/Comments'
+import { Route, useParams } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Sepanta", text: "Watching Anime is Cool" },
+  { id: "q2", author: "Amin", text: "Watching Anime is Terrible :D" },
+];
+const QuoteDetail = (props) => {
+  const params = useParams();
+  console.log(params);
 
-const QuoteDetail = props =>{
-    const params = useParams()
-    console.log(params);
-return <Fragment>
-    <h1>QuoteDetail</h1>
-    <p>{params.quoteId}</p>
-    <Route path={`/quotes/${params.quoteId}/comments`}>
-    <Comments />
-    </Route>
-</Fragment>
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+if(!quote){
+    return <p>Data Not Found!</p>
 }
 
-export default QuoteDetail
+  return (
+    <Fragment>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
