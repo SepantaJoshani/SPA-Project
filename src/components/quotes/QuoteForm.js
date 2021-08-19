8ca48fdff33d5ee3148adfa5a41955e51a7f9e97
Sepanta <Sepanta_97@Yahoo.com>
2021-08-19 21:04:49 +0430

    adding simple validation

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,12 +1,12 @@
-import { Fragment, useRef, useState } from 'react';
-import { Prompt } from 'react-router-dom';
+import { Fragment, useRef, useState } from "react";
+import { Prompt } from "react-router-dom";
 
-import Card from '../UI/Card';
-import LoadingSpinner from '../UI/LoadingSpinner';
-import classes from './QuoteForm.module.css';
+import Card from "../UI/Card";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import classes from "./QuoteForm.module.css";
 
 const QuoteForm = (props) => {
-  const [isEntered, setIsEntered] = useState(false)
+  const [isEntered, setIsEntered] = useState(false);
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
@@ -16,40 +16,61 @@ const QuoteForm = (props) => {
     const enteredAuthor = authorInputRef.current.value;
     const enteredText = textInputRef.current.value;
 
-    // optional: Could validate here
+    const authorIsValid = enteredAuthor.length !== 0;
+    const inpTextIsValid = enteredText.length !== 0;
+    let formIsValid = false;
 
-    props.onAddQuote({ author: enteredAuthor, text: enteredText });
+    if (authorIsValid && inpTextIsValid) {
+      formIsValid = true;
+    }
+
+    if (formIsValid) {
+      props.onAddQuote({ author: enteredAuthor, text: enteredText });
+    }
   }
 
-  const formFocusHandler = ()=>{
-setIsEntered(true)
-  }
+  const formFocusHandler = () => {
+    setIsEntered(true);
+  };
+
+  const finishedFocus = () => {
+    setIsEntered(false);
+  };
 
   return (
-   <Fragment>
-     <Prompt when={isEntered} message={()=>'Are you Sure ? All data Will Be Lost'} />
-     <Card>
-      <form onFocus={formFocusHandler} className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+    <Fragment>
+      <Prompt
+        when={isEntered}
+        message={() => "Are you Sure ? All data Will Be Lost"}
+      />
+      <Card>
+        <form
+          onFocus={formFocusHandler}
+          className={classes.form}
+          onSubmit={submitFormHandler}
+        >
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
 
-        <div className={classes.control}>
-          <label htmlFor='author'>Author</label>
-          <input type='text' id='author' ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='text'>Text</label>
-          <textarea id='text' rows='5' ref={textInputRef}></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
-        </div>
-      </form>
-    </Card>
-   </Fragment>
+          <div className={classes.control}>
+            <label htmlFor="author">Author</label>
+            <input type="text" id="author" ref={authorInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="text">Text</label>
+            <textarea id="text" rows="5" ref={textInputRef}></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button className="btn" onClick={finishedFocus}>
+              Add Quote
+            </button>
+          </div>
+        </form>
+      </Card>
+    </Fragment>
   );
 };
 
