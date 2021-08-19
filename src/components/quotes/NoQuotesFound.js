aa846c87454df903414feea99397fd024c643f1f
Sepanta <Sepanta_97@Yahoo.com>
2021-08-19 18:24:54 +0430

    from-begining

--- src/components/quotes/NoQuotesFound.js
+++ src/components/quotes/NoQuotesFound.js
@@ -1,13 +1,12 @@
-import { Link } from 'react-router-dom';
 import classes from './NoQuotesFound.module.css';
 
 const NoQuotesFound = () => {
   return (
     <div className={classes.noquotes}>
       <p>No quotes found!</p>
-      <Link className='btn' to={'/new-quote'}>
-       Add A Quote
-      </Link>
+      <a className='btn'>
+        Add a Quote
+      </a>
     </div>
   );
 };
--- src/components/quotes/QuoteItem.js
+++ src/components/quotes/QuoteItem.js
@@ -1,4 +1,3 @@
-import { Link} from 'react-router-dom';
 import classes from './QuoteItem.module.css';
 
 const QuoteItem = (props) => {
@@ -10,9 +9,9 @@ const QuoteItem = (props) => {
         </blockquote>
         <figcaption>{props.author}</figcaption>
       </figure>
-      <Link className='btn' to={`/quotes/${props.id}`}>
+      <a className='btn'>
         View Fullscreen
-      </Link>
+      </a>
     </li>
   );
 };
